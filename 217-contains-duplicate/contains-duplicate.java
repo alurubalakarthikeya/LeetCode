5cdbbe56b99ed1550b7980339189d380b1c7970d
Bala Karthikeya Aluru <91084020+alurubalakarthikeya@users.noreply.github.com>
2026-01-29 08:58:26 +0530

    Time: 14 ms (86.02%) | Memory: 93.3 MB (65.36%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,8 +1,18 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+       /* Arrays.sort(nums);
         for(int iter = 0; iter < nums.length-1; iter += 1){
             if(nums[iter]==nums[iter+1]){
+                return true;
+            }
+        } //Bad complexity
+        return false;
+        */
+        Set<Integer> seen = new HashSet<>();
+        for(int iter = 0; iter < nums.length; iter += 1){
+            if(!seen.contains(nums[iter])){
+                seen.add(nums[iter]);
+            }else{
                 return true;
             }
         }
