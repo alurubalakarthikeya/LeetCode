a0666056a960a148774d0f167918a4d6f3488f35
Bala Karthikeya Aluru <91084020+alurubalakarthikeya@users.noreply.github.com>
2026-01-29 17:10:44 +0530

    Time: 20 ms (35.51%) | Memory: 88.2 MB (70.26%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -7,12 +7,34 @@ class Solution {
             }
         } //Bad complexity
         return false;
-        */
         Set<Integer> seen = new HashSet<>();
         for(int iter = 0; iter < nums.length; iter += 1){
             if(!seen.contains(nums[iter])){
                 seen.add(nums[iter]);
             }else{
+                return true;
+            }
+        }
+        return false;
+                */
+        Set<Integer> seen = new HashSet<>();
+        int left = 0, right = nums.length-1;
+        while(left<right){
+            if(!seen.contains(nums[left])){
+                seen.add(nums[left]);
+            }else {
+                return true;
+            }
+            if(!seen.contains(nums[right])){
+                seen.add(nums[right]);
+            }else {
+                return true;
+            }
+            left += 1;
+            right -= 1;
+        }
+        if(nums.length%2!=0){
+            if(seen.contains(nums[left])){
                 return true;
             }
         }
