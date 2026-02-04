9e148633488bdaf3f4a331c79326d9545174250f
Bala Karthikeya Aluru <91084020+alurubalakarthikeya@users.noreply.github.com>
2026-02-04 18:53:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (49.20%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,10 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-        long f = Math.round(
+        int f = (int)(Math.round(
     (Math.pow((1 + Math.sqrt(5)) / 2, n + 1)
    - Math.pow((1 - Math.sqrt(5)) / 2, n + 1))
    / Math.sqrt(5)
-);
-     return (int)f;
+));
+     return f;
     }
 }
