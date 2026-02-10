eb0d5b111203bcc3f8d90d4808e439bd458b194e
Bala Karthikeya Aluru <91084020+alurubalakarthikeya@users.noreply.github.com>
2026-02-10 23:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (68.15%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,12 @@
 class Solution {
-    public static int recursiveSolution(int n){
-        if(n<=1) return n;
-        return recursiveSolution(n-1)+recursiveSolution(n-2);
-    }
     public int fib(int n) {
-        return recursiveSolution(n);
+        if(n<=1) return n;
+        int n1 = 0, n2 = 1;
+        for(int iter = 2; iter <= n; iter += 1){
+            int sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+        }
+        return n2;
     }
 }
