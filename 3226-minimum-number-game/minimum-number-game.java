64b592ba4c75984c3afe1b6e332d15b9b3018e56
Aluru Bala Karthikeya <91084020+alurubalakarthikeya@users.noreply.github.com>
2026-07-28 08:54:08 +0530

    Time: 6 ms (31.75%) | Memory: 46.8 MB (80.09%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -1,14 +1,11 @@
 class Solution {
-    public static void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
     public int[] numberGame(int[] nums) {
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i+=2){
-            swap(nums, i , i+1);
+        int arr[]=new int[nums.length];
+        for(int i=0;i<nums.length;i+=2){
+            arr[i]=nums[i+1];
+            arr[i+1]=nums[i];
         }
-        return nums;
+        return arr;
     }
 }
