f8203061c7a3464a16a1d33e40dcf55f34c5a6a3
Aluru Bala Karthikeya <91084020+alurubalakarthikeya@users.noreply.github.com>
2026-08-03 21:32:15 +0530

    Time: 1 ms (99.73%) | Memory: 46.6 MB (92.38%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[] transformArray(int[] nums) {
         int[] res = new int[nums.length];
+        int eve = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i]%2==0){
+                eve += 1;
+            }
+        }
+        for(int i = 0; i < res.length; i++){
+            if(eve > 0){
                 res[i] = 0;
+                eve -= 1;
             } else {
                 res[i] = 1;
             }
         }
-        Arrays.sort(res);
         return res;
     }
 }
